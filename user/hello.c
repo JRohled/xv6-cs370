a1f0bed10433b397800914d4ef7b674e76704187
Jacob Rohleder <rohlej1@unlv.nevada.edu>
2025-09-09 20:06:19 -0700

    My solution for Project 1 on xv6

---
+++ user/hello.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(){
+
+    printf("Hello World, this is Jacob Rohleder\n");
+    printf("CS370 Section 1002\n");
+    exit(0);
+}
